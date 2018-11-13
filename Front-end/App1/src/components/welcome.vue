4941565e8590e778be309375422e2f9c10dc973d
duonghieu121097 <duonghieu121097@gmail.com>
2018-11-13 22:31:37 +0700

    complete welcome.vue

--- Front-end/App1/src/components/welcome.vue
+++ Front-end/App1/src/components/welcome.vue
@@ -50,4 +50,22 @@ li {
 a {
   color: #42b983;
 }
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
 </style>
